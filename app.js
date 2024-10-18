abf962fdcd1cb7f310237c566d9a35137aab91a2
pragya-celestial-systems <pragya.saxena@celestialsys.com>
2024-10-18 18:05:22 +0530

    added load more button

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ import { displayProducts } from "./js/products.js";
 
 window.addEventListener("DOMContentLoaded", async () => {
   try {
+    // display categories
     const categories = await fetchData("all");
     createCategories(categories);
 
+    // display products
     const products = await fetchData();
     displayProducts(products);
+    document.querySelector('.form-control').addEventListener('input', e => {
+        const filteredProducts = products.filter(product => product.title.includes(e.target.value));
+        console.log(filteredProducts);
+    })
+    // add event listener on search bar 
   } catch (error) {
     console.log(error);
   }
